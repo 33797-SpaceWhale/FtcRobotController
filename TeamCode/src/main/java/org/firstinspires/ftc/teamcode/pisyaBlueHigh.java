7c5923e2eeb2d175821d982e998df433c0149f9a
релтиг <spacewhale33797@gmail.com>
2026-03-17 17:04:59 +0300

    [feat]: переход на ооп

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pisyaBlueHigh.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pisyaBlueHigh.java
@@ -7,26 +7,28 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 @Autonomous (name= "baza BLUEHIGH")
 public class pisyaBlueHigh extends LinearOpMode {
     DcMotor TR, TL, BR, BL;
+    Drivetrain dt = new Drivetrain(this);
+    Shooter sh = new Shooter(this);
 
     @Override
     public void runOpMode() {
-        TR = hardwareMap.dcMotor.get("tr");
-        TL = hardwareMap.dcMotor.get("tl");
-        BR = hardwareMap.dcMotor.get("br");
-        BL = hardwareMap.dcMotor.get("bl");
-
-
-
+       // TR = hardwareMap.dcMotor.get("tr");
+       // TL = hardwareMap.dcMotor.get("tl");
+      //  BR = hardwareMap.dcMotor.get("br");
+       // BL = hardwareMap.dcMotor.get("bl");
 
         waitForStart();
         if (opModeIsActive()) {
-            move(0, -0.4, 0);
-            sleep(500);
-            move(0, 0, 0);
-            //shooter.sh
-            move(0.4, 0, 0);
-            sleep(1000);
-            move(0, 0, 0);
+            dt.moveByDistance(0,1,0,600);
+            sh.shoot(1);
+            dt.moveByDistance(1,0,0,50);
+//            move(0, -0.4, 0);
+//            sleep(500);
+//            move(0, 0, 0);
+//            //shooter.sh
+//            move(0.4, 0, 0);
+//            sleep(1000);
+//            move(0, 0, 0);
         }
     }
 
